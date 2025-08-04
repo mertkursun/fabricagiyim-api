aea464ed654befe7797cf5ebca4e27fb1c56928b
mertkursun <mustafamertkursun@hotmail.com.tr>
2025-08-04 21:40:09 +0300

    fix

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
-const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
+const router = jsonServer.router('../db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1'
+}));
 server.use(router);
 
 module.exports = server;
