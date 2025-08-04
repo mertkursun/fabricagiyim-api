f9162d72019618e16e9b0248b2ad06c9f207b41a
mertkursun <mustafamertkursun@hotmail.com.tr>
2025-08-04 22:21:37 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,10 +2,11 @@ const jsonServer = require('json-server');
 const path = require('path');
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '..', 'db.json')); // ✔️ doğru erişim
+const router = jsonServer.router(path.join(__dirname, '..', 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser); // ✅ Bu satır olmalı!
 server.use(jsonServer.rewriter({
   '/api/*': '/$1'
 }));
