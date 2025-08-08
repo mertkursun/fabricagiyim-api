78e6be0a22b0a4622def7e1f636e17c90d92ccda
mertkursun <mustafamertkursun@hotmail.com.tr>
2025-08-09 00:28:18 +0300

    Update posts.js

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,6 +1,15 @@
 import supabase from '../../lib/supabaseClient.js';
 
 export default async function handler(req, res) {
+   // CORS Headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   if (req.method === 'GET') {
     const { data, error } = await supabase
       .from('posts')
